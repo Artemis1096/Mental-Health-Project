ccfab2832f42a5a4cfd531e7322863b2175ba3ba
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-01-30 08:52:23 +0530

    initial folder structure and files have been set up

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
+import HomeRoutes from "./Routes/HomeRoutes.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-    </Routes>
+    <BrowserRouter>
+      <HomeRoutes />
+    </BrowserRouter>
   );
 }
 
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
 
-import HomeRoutes from "./Routes/HomeRoutes.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <HomeRoutes />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );

e5cb5ca13950f394407a6be69bfc975847b80c4d
aniketcoder003 <aniketcoder003@gmail.com>
2025-02-01 17:34:13 +0530

    get message and message encryption done

--- Backend/Routes/MessageRoutes.js
+++ Backend/Routes/MessageRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { sendMessage } from '../Controllers/MessageController.js'
+import { getMessage, sendMessage } from '../Controllers/MessageController.js'
 import {verify} from '../Utils/WebToken.js'
 const router=express.Router()
 router.post("/send/:id",verify,sendMessage)
+router.get("/get/:id",verify,getMessage)
 
+ 
 export default router

b9f9ebc59937d199e1527dcbe0ee9a24317f57e7
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-02-01 11:47:39 +0530

    Syncing with the Test branch

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 import HomeRoutes from "./Routes/HomeRoutes.jsx";
-import NavBar from "./Components/NavBar.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
       <HomeRoutes />
     </BrowserRouter>
   );

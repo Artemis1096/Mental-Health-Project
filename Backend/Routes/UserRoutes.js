20e3c462737b61d5f59bd00d77ad274ddbaff797
Aryan-Diwakar-1856 <diwakerryan7900@gmail.com>
2025-02-03 19:12:34 +0530

    journal schema and routes created

--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { getUsers, getUser} from '../Controllers/UserController.js';
+import { addJournal, getJournals, deleteJournal } from '../Controllers/JournalController.js';
 import { verify } from '../Utils/WebToken.js';
 
 const router = express.Router();
 
 router.get("/", verify, getUsers);
 router.get("/:id", verify,getUser);
+router.get("/journals/:id", getJournals);
+router.post("/journals/add", addJournal);
+router.delete("/journals/delete/:id", deleteJournal);
 
 export default router;

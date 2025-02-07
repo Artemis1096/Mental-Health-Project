61e9e05cd1d83f22c184a72e047f82ad43ee45f7
Jashanpreet Singh <jashanpreetsingh1096@gmail.com>
2025-02-07 09:23:57 +0530

    Mood Visualization Completed(Testing Pending)

--- FrontEnd/src/Views/Homepage.jsx
+++ FrontEnd/src/Views/Homepage.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "../Styles/HomePage.css";
 import Quotes from "../../Data/quotes.json";
+import MoodComponent from "../Components/MoodComponent";
 
 const Homepage = () => {
   const [todayQuote, setTodayQuote] = useState({ Quote: "", Author: "" });
   const userData = JSON.parse(localStorage.getItem("user"));
-
   useEffect(() => {
     const today = new Date();
     const index = today.getDate() % Quotes.length; // Get a different quote every day
@@ -15,6 +15,7 @@ const Homepage = () => {
   return (
     <div className="main flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-gray-900 to-black text-white px-2">
       {/* Heading */}
+      <MoodComponent />
       <div className="Heading text-center mb-10">
         <p className="text-7xl font-extrabold permanent-marker-regular text-indigo-400 mb-10">
           Welcome
@@ -23,7 +24,6 @@ const Homepage = () => {
           {userData?.name || "Guest"}
         </p>
       </div>
-
       {/* Quote Box */}
       <div className="quote-box bg-purple-700 bg-opacity-90 text-white p-8 md:p-10 rounded-2xl shadow-2xl max-w-2xl w-full text-center">
         <h1 className="text-3xl md:text-4xl font-bold mb-4 permanent-marker-regular">
